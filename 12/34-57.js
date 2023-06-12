fba89b3e019f52a835b05aee871cf463139a0c79
zzznly <zzznly@gmail.com>
2023-06-12 19:27:59 +0900

    12장 예제 진짜 완료

--- 12/34-57.js
+++ 12/34-57.js
@@ -160,27 +160,37 @@ function repeat(n) { // n번만큼 i를 출력하는 함수
 repeat(5); // 0 1 2 3 4
 
 // 12-50
-function callback() {
-    console.log(i);
+// n만큼 어떤 일을 반복한다.
+function repeat1(n) {
+  // i를 출력한다.
+  for (var i = 0; i < n; i++) console.log(i);
 }
-function repeat(n, f) {
-    for (var i = 0; i < n; i++) f();
+
+repeat1(5); // 0 1 2 3 4
+
+// n만큼 어떤 일을 반복한다.
+function repeat2(n) {
+  for (var i = 0; i < n; i++) {
+    // i가 홀수일 때만 출력한다.
+    if (i % 2) console.log(i);
+  }
 }
-repeat(5, callback());
+
+repeat2(5); // 1 3
 
 // 12-51
 function repeat(n, f) {
-  for (var i = 0; i < n; i++) f(i);
+    for (var i = 0; i < n; i++) f(i)
 }
-const logAll = (i) => {
-  console.log(i); // 0, 1, 2, 3, 4
+function logAll(i) {
+    console.log(i);
 }
-repeat(5, logAll);
+repeat(5, logAll); // 0 1 2 3 4 
 
-const logOdds = (i) => {
+function logOdds(i) {
     if(i % 2) console.log(i)
 }
-repeat(5, logOdds); // 1, 3
+repeat(5, logOdds) // 1 3
 
 // 12-52
 // 익명 함수 리터럴을 콜백 함수로 고차 함수에 전달

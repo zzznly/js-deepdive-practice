50213f0e2dacc0d94189a8fbb9e6b97f1ed1304c
zzznly <zzznly@gmail.com>
2023-06-24 23:46:02 +0900

    16, 17장 예제

--- 16/01-14.js
+++ 16/01-14.js
@@ -1,4 +1,102 @@
 // 16-01
 const o = {};
+
+// 내부 슬롯 : 자바스크립트 엔진의 내부 로직이므로 직접 접근 불가!
 o.[[Prototype]] // -> Uncaught SyntaxError: Unexpected token '['
+
+// 단, 일부 내부 슬롯과 내부 메서드에 한하여 간접적으로 접근 가능한 수단을 제공하긴 함
 o.__proto__ // -> Object.prototype
+
+
+// 16-02
+const person2 = {
+    name: 'Lee',
+};
+// 프로퍼티 어트리뷰트 정보를 제공하는 프로퍼티 디스크립터 객체를 반환
+console.log(Object.getOwnPropertyDescriptor(person2, 'name'));
+// {value: "Lee", writable: true, enumerable: true, configurable: true}
+
+
+// 16-03
+const person3 = {
+    name: 'Lee',
+}
+// 프로퍼티 동적 생성
+person3.age = 20;
+
+// 모든 프로퍼티의 프로퍼티 어트리뷰트 정보를 제공하는 프로퍼티 디스크립터 객체들을 반환한다.
+console.log(Object.getOwnPropertyDescriptors(person3));
+/*
+{
+  name: {value: "Lee", writable: true, enumerable: true, configurable: true},
+  age: {value: 20, writable: true, enumerable: true, configurable: true}
+}
+*/
+
+
+// 16-04
+const person4 = {
+    name: "Lee",
+};
+// 프로퍼티 어트리뷰트 정보를 제공하는 프로퍼티 디스크립터 객체를 취득한다.
+console.log(Object.getOwnPropertyDescriptor(person4, "name"));
+// {value: "Lee", writable: true, enumerable: true, configurable: true}
+
+
+// 16-05
+const person5 = {
+    name: 'Lee',
+};
+
+// 프로퍼티 동적 생성
+person5.age = 20;
+console.log(Object.getOwnPropertyDescriptors(person5));
+/*
+{
+  name: {value: "Lee", writable: true, enumerable: true, configurable: true},
+  age: {value: 20, writable: true, enumerable: true, configurable: true}
+}
+*/
+
+
+// 16-06
+const person = {
+  // 데이터 프로퍼티
+  firstName: "Ungmo",
+  lastName: "Lee",
+
+  // fullName은 접근자 함수로 구성된 접근자 프로퍼티다.
+  // getter 함수
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  // setter 함수
+  set fullName(name) {
+    // 배열 디스트럭처링 할당: "31.1 배열 디스트럭처링 할당" 참고
+    [this.firstName, this.lastName] = name.split(" ");
+  },
+};
+
+// 데이터 프로퍼티를 통한 프로퍼티 값의 참조.
+console.log(person.firstName + " " + person.lastName); // Ungmo Lee
+
+// 접근자 프로퍼티를 통한 프로퍼티 값의 저장
+// 접근자 프로퍼티 fullName에 값을 저장하면 setter 함수가 호출된다.
+person.fullName = "Heegun Lee";
+console.log(person); // {firstName: "Heegun", lastName: "Lee"}
+
+// 접근자 프로퍼티를 통한 프로퍼티 값의 참조
+// 접근자 프로퍼티 fullName에 접근하면 getter 함수가 호출된다.
+console.log(person.fullName); // Heegun Lee
+
+// firstName은 데이터 프로퍼티다.
+// 데이터 프로퍼티는 [[Value]], [[Writable]], [[Enumerable]], [[Configurable]] 프로퍼티 어트리뷰트를 갖는다.
+let descriptor = Object.getOwnPropertyDescriptor(person, "firstName");
+console.log(descriptor);
+// {value: "Heegun", writable: true, enumerable: true, configurable: true}
+
+// fullName은 접근자 프로퍼티다.
+// 접근자 프로퍼티는 [[Get]], [[Set]], [[Enumerable]], [[Configurable]] 프로퍼티 어트리뷰트를 갖는다.
+descriptor = Object.getOwnPropertyDescriptor(person, "fullName");
+console.log(descriptor);
+// {get: ƒ, set: ƒ, enumerable: true, configurable: true}

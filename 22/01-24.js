f294515e9784290bad960e4fbad78cde910e7898
zzznly <zzznly@gmail.com>
2023-07-16 03:35:14 +0900

    22장 예제 연습 완료

--- 22/01-24.js
+++ 22/01-24.js
@@ -38,7 +38,7 @@ Circle.prototype.getDiameter = function () {
 };
 
 const c1 = new Circle(5);
-console.log(circle.getDiameter()); // 10
+console.log(c1.getDiameter()); // 10
 
 // 05
 console.log(this); // window(전역 객체)
@@ -90,6 +90,7 @@ foo.apply(bar); // bar
 foo.bind(bar); // bar
 
 // 07
+// * 중첩함수를 일반 함수로 호출하면 함수 내부의 this에는 전역 객체가 바인딩된다
 function foo() {
   console.log("foo's this", this); // window
 
@@ -186,3 +187,152 @@ const obj4 = {
 };
 
 obj4.foo();
+
+// 13
+var value = 1;
+
+const obj5 = {
+  value: 100,
+  foo() {
+    // 화살표 함수의 this는 상위 스코프의 this를 가리킴
+    setTimeout(() => console.log(this.value), 100); // 100
+  },
+};
+
+obj5.foo();
+
+// 14
+const person1 = {
+  name: "Lee",
+  getName() {
+    // 메서드 내부의 this는 메서드를 호출한 객체에 바인딩
+    return this.name;
+  },
+};
+
+// 메서드 getName을 호출한 객체는 person
+console.log(person1.getName()); // Lee
+
+// 15
+const anotherPerson = {
+  name: "Kim",
+};
+anotherPerson.getName = person1.getName;
+
+console.log(anotherPerson.getName()); // Kim
+
+const getName = person1.getName;
+
+console.log(getName()); // 일반함수로 호출
+// window.name - '' // Node.js - undefined
+
+// 16
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.getName = function () {
+  return this.name;
+};
+
+const me2 = new Person("Lee");
+
+console.log(me2.getName()); // Lee
+
+Person.prototype.name = "Kim";
+
+console.log(Person.prototype.getName()); // Kim
+
+// 17
+function Circle(radius) {
+  this.radius = radius;
+  this.getDiameter = function () {
+    return 2 * this.radius;
+  };
+}
+
+const c2 = new Circle(5);
+const c3 = new Circle(10);
+
+console.log(c2.getDiameter()); // 10
+console.log(c3.getDiameter()); // 20
+
+// 18
+const c4 = Circle(15); // new 연산자와 함께 호출되지 않음
+
+console.log(c4); // undefined
+console.log(radius); // 15
+
+// 19
+function getThisBinding() {
+  return this;
+}
+
+const thisArg = { a: 1 };
+
+console.log(getThisBinding()); // window
+console.log(getThisBinding.apply(thisArg)); // {a : 1}
+
+// 20
+function getThisBinding() {
+  console.log(arguments);
+  return this;
+}
+
+const thisArg1 = { a: 1 };
+
+console.log(getThisBinding.apply(thisArg1, [1, 2, 3])); // Arguments(3) [1,2,3] // { a : 1 }
+console.log(getThisBinding.call(thisArg1, 1, 2, 3)); // Arguments(3) [1,2,3] // { a : 1 }
+
+// 21
+function convertArgsToArray() {
+  console.log(arguments);
+
+  // ** Array.prototype.slice 를 인수 없이 호출하면 배열의 복사본 생성!
+  const arr = Array.prototype.slice.call(arguments);
+
+  console.log(arr);
+
+  return arr;
+}
+
+convertArgsToArray(1, 2, 3); // [1,2,3]
+
+// 22
+function getThisBinding() {
+  return this;
+}
+
+const thisArg2 = { a: 1 };
+
+/** bind() 메서드는 첫번째 인수로 전달한 thisArg 로 this 바인딩이 교체된
+ * getThisBinding 함수를 새롭게 생성해 반환한다 */
+console.log(getThisBinding.bind(thisArg)); // getThisBinding
+console.log(getThisBinding.bind(thisArg)()); // { a : 1 }
+
+// 23
+const person2 = {
+  name: "Lee",
+  foo(callback) {
+    setTimeout(callback, 100);
+  },
+};
+
+person2.foo(function () {
+  console.log(`Hi my name is ${this.name}`); // window.name
+});
+
+// 브라우저 환경 ('') - Hi my name is
+// Node.js 환경 (undefined) - Hi my name is undefined
+
+// 24
+const person3 = {
+  name: "Lee",
+  foo(callback) {
+    setTimeout(callback.bind(this), 100); // this 는 person3 객체
+  }, // bind() 메서드가 person3이 this인 callback 함수를 새롭게 생성해 반환
+};
+
+person3.foo(function () {
+  console.log(`Hi my name is ${this.name}`); // person3.name // Hi my name is Lee
+});

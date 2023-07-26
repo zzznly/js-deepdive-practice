233abdfc689c9aad210389f0e5861a62836efad8
zzznly <zzznly@gmail.com>
2023-07-26 10:03:04 +0900

    24장 12번까지

--- 24/01-23.js
+++ 24/01-23.js
@@ -82,3 +82,115 @@ innerFunc(); // (4) 10
 
 // => 이처럼 외부함수보다 중첩 함수가 더 오래 유지되는 경우, 중첩 함수가 이미 종료됐을때 외부 함수의 변수 참조 가능
 // => 이러한 중첩 함수를 "클로저" 라고 한다! ***
+
+// 자바스크립트의 모든 함수는 상위 스코프를 기억하므로 이론적으로 모든 함수는 클로저다
+
+// 06
+function foo() {
+  const x = 1;
+  const y = 2;
+
+  // 일반적으로 클로저라고 하지 않는다.
+  function bar() {
+    const z = 3;
+
+    debugger;
+    // 상위 스코프의 식별자를 참조하지 않는다. -> 상위 스코프 기억하지 않는다 (참조하지 않는 식별자를 기억하면 메모리 낭비!)
+    console.log(z); // 3
+  }
+
+  return bar;
+}
+
+const bar = foo();
+bar();
+
+// 07
+function foo2() {
+  const x = 1;
+
+  // 상위 스코프의 식별자 참조하므로 클로저다!
+  // bar 함수는 클로저였지만 곧바로 소멸한다.
+  function bar2() {
+    debugger;
+    // 상위 스코프의 식별자를 참조한다.
+    console.log(x);
+  }
+  bar2();
+}
+// 외부함수 foo2 외부로 중첩함수 bar2가 반환되지 않음 => bar2 의 생명주기가 foo2 보다 짧음
+// => 클로저가 아님
+
+foo2();
+
+// 08
+// 클로저
+// 중접함수 bar3은 외부 함수 foo3 보다 생명주기 길고, 상위 스코프 식별자를 참조
+function foo3() {
+  const x = 1;
+  const y = 2;
+
+  function bar3() {
+    console.log(x); // 1
+  }
+  return bar3;
+}
+const bar3 = foo3();
+bar3();
+
+// 09
+let num = 0;
+
+const increase = function () {
+  return ++num;
+};
+
+console.log(increase()); // 1
+console.log(increase()); // 2
+console.log(increase()); // 3
+
+// 10
+const increase2 = function () {
+  let num = 0;
+
+  return ++num;
+};
+
+console.log(increase2()); // 1
+console.log(increase2()); // 1
+console.log(increase2()); // 1
+
+// 11
+const increase3 = (function () {
+  let num = 0;
+
+  return function () {
+    return ++num;
+  };
+})();
+
+console.log(increase3()); // 1
+console.log(increase3()); // 2
+console.log(increase3()); // 3
+
+// => 클로저는 상태가 의도치 않게 변하는 것을 막기 위해 안전하게 은닉하는 용도로 사용!
+
+// 12
+const counter = (function () {
+  let num = 0;
+
+  return {
+    increase() {
+      return ++num;
+    },
+    decrease() {
+      return num > 0 ? --num : 0;
+    },
+  };
+})();
+
+console.log(counter.increase()); // 1
+console.log(counter.increase()); // 2
+
+console.log(counter.decrease()); // 1
+console.log(counter.decrease()); // 0
